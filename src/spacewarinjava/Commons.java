9635dc127b1c8774f10e2fc87fb2405467c86a8a
Aysenur1958-wq <64094038+Aysenur1958-wq@users.noreply.github.com>
2020-12-24 10:36:39 +0300

    Update Commons.java

--- src/spacewarinjava/Commons.java
+++ src/spacewarinjava/Commons.java
@@ -4,12 +4,12 @@ package spacewarinjava;
 
 public interface Commons {
     
-    int BOARD_WIDTH = 358;
-    int BOARD_HEIGHT = 350;
-    int BORDER_RIGHT = 30;
+    int BOARD_WIDTH = 300;
+    int BOARD_HEIGHT = 300;
+    int BORDER_RIGHT = 25;
     int BORDER_LEFT = 5;
 
-    int GROUND = 290;
+    int GROUND = 270;
     int BOMB_HEIGHT = 5;
 
     int ALIEN_HEIGHT = 12;
@@ -20,7 +20,7 @@ public interface Commons {
     int GO_DOWN = 15;
     int NUMBER_OF_ALIENS_TO_DESTROY = 24;
     int CHANCE = 5;
-    int DELAY = 17;
-    int PLAYER_WIDTH = 15;
+    int DELAY = 10;
+    int PLAYER_WIDTH = 10;
     int PLAYER_HEIGHT = 10;
 }
